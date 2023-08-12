7f7e90a2cce3a018ed6271c197efd0dd95edf47e
Pavan <pt426@cam.ac.uk>
2023-08-12 11:09:47 +0100

    Fixed initial conditions.

--- initial_condition_files/slurm_template.sh
+++ initial_condition_files/slurm_template.sh
@@ -9,12 +9,11 @@
 #SBATCH --mail-type=ALL
 #SBATCH --time=0-12:00:00
 
+module load hdf5/1.12.1
 module load libtool-2.4.6-gcc-5.4.0-xtclmhg
 module load fftw-3.3.6-pl2-gcc-5.4.0-rn4yojp
-module load metis-5.1.0-gcc-5.4.0-63vpksi
-module load gsl-2.3-gcc-5.4.0-lqjrzui
+module load metis-5.1.0-gcc-5.4.0-pnrgiky
+module load gsl-2.4-gcc-5.4.0-z4fspad
 module load jemalloc-4.5.0-gcc-5.4.0-j3zbugm
-module load python/3.8
-module load hdf5/1.8.8
 
 ../swiftsim/swift -s -G -t 70 simulation_parameters.yml 2>&1 | tee output.log

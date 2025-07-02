592ea5042eb53b62b0a83766482e54289f9803bf
Pavan <pt426@cam.ac.uk>
2025-07-02 16:48:45 +0100

    Filled out dPdr solver

--- photosphere-2.py
+++ photosphere-2.py
@@ -4,6 +4,8 @@
 import jax.numpy as jnp
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline
+from scipy.integrate import solve_ivp
 
 from swiftsimio.visualisation import slice_gas
 from unyt import Rearth
@@ -11,18 +13,18 @@ from unyt import Rearth
 from snapshot_analysis import snapshot
 import EOS as fst
 
-# constants and function definitions
 sigma = 5.670374419e-8 # stefan-boltzmann constant
 L_sun = 3.828e26 # solar luminosity in watts
 R_earth = 6371000 # radius of Earth in meters
 M_earth = 5.972e24 # mass of Earth in kg
+G = 6.67430e-11 # gravitational constant in m^3 kg^-1 s^-2
 day = 3600 * 24
 yr = 365.25 * day
 silicate_latent_heat_v = 3e7
 photosphere_depth = 2/3
-pi = jnp.pi
-cos = lambda theta: jnp.cos(theta)
-sin = lambda theta: jnp.sin(theta)
+pi = np.pi
+cos = lambda theta: np.cos(theta)
+sin = lambda theta: np.sin(theta)
 
 class photosphere:
 
@@ -33,7 +35,7 @@ class photosphere:
 
         resolution = 1000
         sample_size = 10 * Rearth
-        max_size = 50 * Rearth
+        max_size = 50 * 6371000
 
         # calculate the center of the snapshot and set the limits for the slice
         center = self.snapshot.center_of_mass
@@ -84,26 +86,65 @@ class photosphere:
         densities = mass_slice[tuple(indexes)]
         temperatures = get_slice('temperatures')
         pressures, entropies = get_slice('pressures'), get_slice('entropy')
-        angular_momenta = get_slice('specific_angular_momentum')
 
         # convert data to MKS
         temperatures.convert_to_mks()
         pressures.convert_to_mks()
         entropies.convert_to_mks()
-        angular_momenta.convert_to_mks()
         r_range.convert_to_mks()
 
-        self.rho = jnp.array(np.mean(densities, axis=0))
-        self.T = jnp.array(np.mean(temperatures, axis=0))
-        self.P = jnp.array(np.mean(pressures, axis=0))
-        self.s = jnp.array(np.mean(entropies, axis=0))
-        self.L = jnp.array(np.mean(angular_momenta, axis=0))
-        self.r = jnp.array(r_range)
+        self.rho = np.array(np.mean(densities, axis=0))
+        self.T = np.array(np.mean(temperatures, axis=0))
+        self.P = np.array(np.mean(pressures, axis=0))
+        self.s = np.array(np.mean(entropies, axis=0))
+        self.r = np.array(r_range)
 
-        # extrapolate entropy here !
+        r_extension = np.linspace(self.r[-1], max_size, num=resolution // 2)
+
+        self.r = np.concatenate((self.r, r_extension))
+        self.rho = np.concatenate((self.rho, np.zeros_like(r_extension)))
+        self.T = np.concatenate((self.T, np.zeros_like(r_extension)))
+        self.P = np.concatenate((self.P, np.zeros_like(r_extension)))
+        self.s = np.concatenate((self.s, np.zeros_like(r_extension)))
+
+        self.extrapolate_r = self.snapshot.HD_limit_R.value * 6371000  # density floor limit in meters
+        self.extrapolation_index = np.argmax(self.r > self.extrapolate_r)
+        s_extrapolation_value = self.s[self.extrapolation_index - 1]
+
+        self.s = np.where(self.r < self.extrapolate_r, self.s, s_extrapolation_value)
+        self.s_interpolation = CubicSpline(self.r, self.s)
+
+        plt.plot(self.r, self.s)
+        plt.savefig("entropy_profile.png")
+        plt.close()
 
     def solve_dPdr(self):
-        pass
+        
+        def dPdr(r, P):
+            
+            s = self.s_interpolation(r)
+            omega = self.snapshot.best_fit_rotation_curve_mks(r)
+            M = self.snapshot.total_mass 
+
+            rho = fst.rho_EOS(s, P)
+            gravity = - G * M / (r ** 2)
+            centrifugal = r * omega ** 2
+
+            return - rho * (gravity + centrifugal)
+        
+        solution = solve_ivp(
+            dPdr,
+            (self.extrapolate_r, self.r[-1]),
+            [self.P[self.extrapolation_index - 1]],
+            t_eval=self.r[self.extrapolation_index:]
+            )
+        
+        self.P[self.extrapolation_index:] = solution.y[0]
+        
+
+
+
+
 
     def calculate_EOS(self):
         pass

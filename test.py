31d6b5a399f11b35d3e469822d8031bc3dbfb282
astro-pavan <pt426@cam.ac.uk>
2025-09-05 17:49:31 +0100

    Updated plots

--- test.py
+++ test.py
@@ -146,11 +146,11 @@ def results_plot_and_table():
 
         L0_10.append(p1.L_phot)
         t, L, R, T, t_half, t_tenth = p1.cool(80 * yr, n=10000)
-        plt.plot(t / yr, L / L_sun)
-        plt.ylim([0, L[0] / L_sun])
-        plt.title(i)
-        plt.savefig(f'{i}.png')
-        plt.close()
+        # plt.plot(t / yr, L / L_sun)
+        # plt.ylim([0, L[0] / L_sun])
+        # plt.title(i)
+        # plt.savefig(f'{i}.png')
+        # plt.close()
 
         # final_mass[i] = p1.snapshot.total_mass / M_earth
         # final_AM[i] = p1.snapshot.total_angular_momentum
@@ -206,19 +206,38 @@ def results_plot_and_table():
     plt.savefig('figures/big_plot_v2.pdf', bbox_inches='tight')
     plt.close()
 
+    print('INITIAL CONDITIONS TABLE:')
+
+    print('\multicolumn{2}{c|}{Simulation} & \multicolumn{5}{c}{Initial Parameters}\\\\')
+    print('Set & \# & No. of particles & $M_{\mathrm{total}}$ / $M_{\oplus}$ & $\gamma$ & $b$ & $v_{\mathrm{imp}}$ / $v_{\mathrm{esc}}$ \\\\ \hline')
+
     for j in range(len(index)):
         i = index[j]
+        print(' & ', end='')
         print(f'{j} & ', end='')
         print(f'{n_particles[i]:.0f} & ', end='')
         print(f'{total_mass[i]:.2f} & ', end='')
         print(f'{mass_ratio[i]:.2f} & ', end='')
         print(f'{impact_parameter[i]:.2f} & ', end='')
-        print(f'{v_over_v_esc[i]:.1f} & ', end='')
+        print(f'{v_over_v_esc[i]:.1f} \\\\')
+
+    print('RESULTS TABLE:')
+
+    print('\multicolumn{2}{c|}{Simulation} & \multicolumn{2}{c|}{} & \multicolumn{2}{c|}{P = 3 day} & \multicolumn{2}{c|}{P = 30 days} & \multicolumn{2}{c}{P = 300 days} \\\\')
+    print('Set & \# & $M_{\mathrm{final}}$ / $M_{\oplus}$ & $AM_{\mathrm{final}}$ / $10^{34}$ $\\text{kg}\, \\text{m}^2\\text{s}^{-1}$ & $L_{0}$ / $10^{-3}L_{\odot}$ & $t_{\mathrm{cool}}$ (days) & $L_{0}$ / $10^{-3}L_{\odot}$ & $t_{\mathrm{cool}}$ (days) & $L_{0}$ / $10^{-3}L_{\odot}$ & $t_{\mathrm{cool}}$ (days) \\\\ \hline')
+
+    for j in range(len(index)):
+        i = index[j]
+        print(' & ', end='')
+        print(f'{j} & ', end='')
         print(f'{final_mass[i] / M_earth:.2f} & ', end='')
         print(f'{final_AM[i] / 1e34:.2f} & ', end='')
-        print(f'{L0_10[i] / 0.001:.1f} & ', end='')
-        print(f'{t_cool_10[i] / day:.0f} \\\\')
-
+        print(f'{L0_1[j] / 0.001:.1f} & ', end='')
+        print(f'{t_cool_1[j] / day:.0f} & ', end='')
+        print(f'{L0_10[j] / 0.001:.1f} & ', end='')
+        print(f'{t_cool_10[j] / day:.0f} & ', end='')
+        print(f'{L0_100[j] / 0.001:.1f} & ', end='')
+        print(f'{t_cool_100[j] / day:.0f} \\\\')
 
 def phase_diagram():
     
@@ -325,17 +344,17 @@ def results_no_droplet_removal():
     for i in tqdm([0, 1, 2, 3, 4, 5, 6, 7]):
 
         index.append(i)
-        p1 = photosphere(get_filename(i, 4))
-        p1_no_remove = photosphere(get_filename(i, 4), remove_droplets=False)
+        p1 = photosphere(get_filename(i, 4), orbital_period=30 * day)
+        p1_no_remove = photosphere(get_filename(i, 4), remove_droplets=False, orbital_period=30 * day)
 
         total_m.append(total_mass[i])
 
         L0.append(p1.L_phot)
         L0_no_remove.append(p1_no_remove.L_phot)
 
-        t, L, R, T, t_half, t_tenth = p1.cool(20 * yr, n=10000)
+        t, L, R, T, t_half, t_tenth = p1.cool(80 * yr, n=10000)
         t_cool.append(t_half)
-        t, L, R, T, t_half, t_tenth = p1_no_remove.cool(20 * yr, n=10000)
+        t, L, R, T, t_half, t_tenth = p1_no_remove.cool(80 * yr, n=10000)
         t_cool_no_remove.append(t_half)
 
     L0 = np.array(L0) / L_sun
@@ -356,8 +375,8 @@ def results_no_droplet_removal():
 
     plt.legend()
 
-    #plt.xlim([0, (np.max(t_cool_no_remove) / day) + 50])
-    plt.xlim([0.1, 10000])
+    plt.xlim([0, (np.max(t_cool_no_remove) / day) + 50])
+    # plt.xlim([0.1, 10000])
 
     plt.xlabel('Cooling time (day)')
     plt.ylabel('Initial luminosity ($L_{\odot}$)')
@@ -428,26 +447,114 @@ def orbital_period_plot():
     plt.savefig('figures/hill_plot.pdf', bbox_inches='tight')
     plt.close()
 
+def orbital_period_plot_v2():
+
+    index = []
+    total_m = []
+
+    L0_1 = []
+    t_cool_1 = []
+
+    indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+    for i in tqdm(indexes):
+
+        index.append(i)
+        p1 = photosphere(get_filename(i, 4), orbital_period=3 * day)
+
+        L0_1.append(p1.L_phot)
+        t, L, R, T, t_half, t_tenth = p1.cool(20 * yr, n=10000)
+        t_cool_1.append(t_half)
+
+    L0_1 = np.array(L0_1) / L_sun
+    t_cool_1 = np.array(t_cool_1)
+
+    L0_10 = []
+    t_cool_10 = []
+
+    for i in tqdm(indexes):
+
+        p1 = photosphere(get_filename(i, 4), orbital_period=30 * day)
+
+        L0_10.append(p1.L_phot)
+        t, L, R, T, t_half, t_tenth = p1.cool(80 * yr, n=10000)
+        # plt.plot(t / yr, L / L_sun)
+        # plt.ylim([0, L[0] / L_sun])
+        # plt.title(i)
+        # plt.savefig(f'{i}.png')
+        # plt.close()
+
+        # final_mass[i] = p1.snapshot.total_mass / M_earth
+        # final_AM[i] = p1.snapshot.total_angular_momentum
+        # total_m.append(total_mass[i])
+
+        t_cool_10.append(t_half)
+
+    L0_10 = np.array(L0_10) / L_sun
+    t_cool_10 = np.array(t_cool_10)
+
+    L0_100 = []
+    t_cool_100 = []
+
+    for i in tqdm(indexes):
+
+        p1 = photosphere(get_filename(i, 4), orbital_period=300 * day)
+
+        L0_100.append(p1.L_phot)
+        t, L, R, T, t_half, t_tenth = p1.cool(20 * yr, n=10000)
+        t_cool_100.append(t_half)
+
+    L0_100 = np.array(L0_100) / L_sun
+    t_cool_100 = np.array(t_cool_100)
+
+    plt.figure(figsize=(8, 6), dpi=300)
+
+    scatter1 = plt.scatter(t_cool_1 / day, L0_1, s=20,
+                           c=indexes, cmap='tab20', marker='o', label='Period = 3 days')
+    plt.scatter(t_cool_10 / day, L0_10, s=20,
+                c=indexes, cmap='tab20', marker='x', label='Period = 30 days')
+    plt.scatter(t_cool_100 / day, L0_100, s=20,
+                c=indexes, cmap='tab20', marker='+', label='Period = 300 days')
+
+    # plt.colorbar(scatter1, label='Simulation Number')
+    # plt.clim(0, 4.0)
+
+    # plt.xlim([0, (np.max(t_cool) / day) + 50])
+    plt.xlim([2, 10000])
+    plt.ylim([1e-5, 2e-2])
+
+    plt.xlabel('Cooling time (day)')
+    plt.ylabel('Initial luminosity ($L_{\odot}$)')
+
+    plt.yscale('log')
+    plt.xscale('log')
+
+    # plt.axvline(30, linestyle='--', color='black', label='Gaia average cadence')
+    # plt.axvline(3, linestyle='--', color='red', label='LSST average cadence')
+
+    plt.legend()
+
+    plt.savefig('figures/hill_plot_v2.png', bbox_inches='tight')
+    plt.savefig('figures/hill_plot_v2.pdf', bbox_inches='tight')
+    plt.close()
+
 def pressure_floor_plot():
     
-    impact_indexes = [2, 0, 5, 7]
-    impact_labels = ['0.5', '1.0', '2.0', '4.0']
+    impact_indexes = [0, 5, 7]
+    impact_labels = ['1.0', '2.0', '4.0']
 
     pressures = [1e15, 1e13, 1e11, 1e9, 1e7]
 
-    fig, ax = plt.subplots()
-    plt.subplots_adjust(hspace=0)
-    fig.set_figwidth(6.4)
-    fig.set_figheight(5)
+    plt.figure(figsize=(6.4, 5))
 
     for i in tqdm(range(len(impact_indexes))):
 
         L0 = []
         t_cool = []
 
-        for P in tqdm(pressures):
+        for P in pressures:
 
-            print(f'{i}: {P}')
+            # print(f'{i}: {P}')
 
             filename = get_filename(impact_indexes[i], 4)
 
@@ -455,24 +562,30 @@ def pressure_floor_plot():
 
             L0.append(phot.L_phot / L_sun)
 
-            t, L, R, T, t_half, t_tenth = phot.cool(20 * yr, n=10000)
+            t, L, R, T, t_half, t_tenth = phot.cool(160 * yr, n=10000)
             t_cool.append(t_half / day)
+
+            # plt.plot(t / yr, L / L_sun)
+            # plt.ylim([0, L[0] / L_sun])
+            # plt.title(f'{P:.1e}-{impact_labels[i]}.png')
+            # plt.savefig(f'{P:.1e}-{impact_labels[i]}.png')
+            # plt.close()
 
         label1 = impact_labels[i]
         colour = viridis(i / len(impact_indexes))
 
-        ax.scatter(pressures, t_cool, marker='o', c=colour, label=label1)
+        plt.scatter(pressures, t_cool, marker='o', c=colour, label=label1)
 
-    ax.set_yscale('log')
-    ax.set_xscale('log')
+    plt.yscale('log')
+    plt.xscale('log')
 
-    ax.set_xlabel('Inner pressure limit (Pa)')
-    ax.set_ylabel('Cooling time (days)')
-    ax.legend(title='Total mass ($M_{\oplus}$)')
+    plt.xlabel('Inner pressure limit (Pa)')
+    plt.ylabel('Cooling time (days)')
+    plt.legend(title='Total mass ($M_{\oplus}$)')
 
     plt.savefig('figures/pressure_plot.png', bbox_inches='tight')
     plt.savefig('figures/pressure_plot.pdf', bbox_inches='tight')
     plt.close()
 
 if __name__ == '__main__':
-    results_plot_and_table()
+    orbital_period_plot_v2()
